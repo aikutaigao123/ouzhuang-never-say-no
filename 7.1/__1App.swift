d4815e0ea1aab344b083afc1e62fc21f5db175c0
DieChen <928322941@qq.com>
2025-07-01 17:24:34 +0800

    Initial Commit

---
+++ 7.1/__1App.swift
@@ -0,0 +1,17 @@
+//
+//  __1App.swift
+//  7.1
+//
+//  Created by Die chen on 2025/7/1.
+//
+
+import SwiftUI
+
+@main
+struct __1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 7.1Tests/__1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  __1Tests.swift
+//  7.1Tests
+//
+//  Created by Die chen on 2025/7/1.
+//
+
+import Testing
+@testable import __1
+
+struct __1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
